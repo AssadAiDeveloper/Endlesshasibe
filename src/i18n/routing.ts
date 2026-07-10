4d387e08900fd2868dae630de8e24b0b02763976
Salim Assad <s.assad.ye@gmail.com>
2026-07-10 14:49:31 +0200

    Add src/i18n/routing.ts

---
+++ src/i18n/routing.ts
@@ -0,0 +1,11 @@
+import { defineRouting } from "next-intl/routing";
+import { createNavigation } from "next-intl/navigation";
+
+export const routing = defineRouting({
+  locales: ["nl", "en", "ar"],
+  defaultLocale: "nl",
+  localePrefix: "always",
+});
+
+export const { Link, redirect, usePathname, useRouter, getPathname } =
+  createNavigation(routing);
